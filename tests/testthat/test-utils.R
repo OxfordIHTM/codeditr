96ca19b475dfc5a3fa558ec13250c68271d383d5
Ernest Guevarra <ernest@guevarra.io>
2024-06-27 11:09:19 +0100

    test utils funcdtions

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -10,3 +10,17 @@ testthat::test_that(
     "list"
   )
 )
+
+
+testthat::test_that(
+  "list_ill_defined_icd11 outputs appropriate results",
+  testthat::expect_vector(list_ill_defined_icd11())
+)
+
+
+code_range <- c("A71.0-A71.9", "F50.1,F50.3-F50.9")
+
+testthat::test_that(
+  "expand_icd10_code_range outputs appropriate results",
+  testthat::expect_vector(expand_icd10_code_range(code_range))
+)
