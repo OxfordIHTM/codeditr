5e4355765bd47c2331ad2cb4948fd3c00ed29ba4
Ernest Guevarra <ernest@guevarra.io>
2024-06-27 20:43:57 +0100

    complete tests for utils

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -3,37 +3,20 @@
 age <- c(1, 20, 45, 63, 125)
 age_type <- c("Y", "M", "D", "M", "Y")
 
-testthat::test_that(
-  "get_age_values outputs appropriate results",
+testthat::test_that("get_age_values outputs appropriate results", {
   testthat::expect_type(
     get_age_values(age[1], age_type[1]),
     "list"
   )
-)
+})
 
-testthat::test_that(
-  "get_age_values warns appropriately",
-  testthat::expect_warning(
-    get_age_values(age[5], age_type[5])
-  )
-)
+testthat::test_that("get_age_values warns appropriately", {
+  testthat::expect_warning(get_age_values(age[5], age_type[5]))
+  testthat::expect_warning(get_age_values(age[3], age_type[3]))
+  testthat::expect_warning(get_age_values(age[2], age_type[2]))
+})
 
-testthat::test_that(
-  "get_age_values warns appropriately",
-  testthat::expect_warning(
-    get_age_values(age[3], age_type[3])
-  )
-)
-
-testthat::test_that(
-  "get_age_values warns appropriately",
-  testthat::expect_warning(
-    get_age_values(age[2], age_type[2])
-  )
-)
-
-testthat::test_that(
-  "get_score_combo outputs appropriate results",
+testthat::test_that("get_score_combo outputs appropriate results", {
   testthat::expect_s3_class(
     get_score_combo(
       scores = c(1, 2, 3, 4, 5),
@@ -41,17 +24,24 @@ testthat::test_that(
     ),
     "tbl"
   )
-)
+})
 
-testthat::test_that(
-  "list_ill_defined_icd11 outputs appropriate results",
+testthat::test_that("get_score_combo errors appropriately", {
+  testthat::expect_error(
+    get_score_combo(
+      scores = c(1, 2, 3, 4, 5),
+      labels = c("A", "B", "C", "D")
+    )
+  )
+})
+
+testthat::test_that("list_ill_defined_icd11 outputs appropriate results", {
   testthat::expect_vector(list_ill_defined_icd11())
-)
+})
 
 
 code_range <- c("A71.0-A71.9", "F50.1,F50.3-F50.9")
 
-testthat::test_that(
-  "expand_icd10_code_range outputs appropriate results",
+testthat::test_that("expand_icd10_code_range outputs appropriate results", {
   testthat::expect_vector(expand_icd10_code_range(code_range))
-)
+})
