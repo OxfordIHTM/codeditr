24c7d6a5a37a7b4ecf31add294eb97707ec668ad
Ernest Guevarra <ernest@guevarra.io>
2024-06-28 06:40:00 +0100

    structure CoDEdit input dataset

--- R/cod_check_cause.R
+++
@@ -1,6 +0,0 @@
-#'
-#' Check cause of death code plausibility against sex
-#'
-#' @param cod A character value
-#'
-#'
--- data-raw/codedit_database.R
+++ data-raw/codedit_database.R
@@ -6,9 +6,17 @@ library(openxlsx2)
 ## Read example death records datasets ----
 
 ### ICD10 example ----
-icd10_example <- openxlsx2::read_xlsx(file = "data-raw/data_example_icd10.xlsx")
+icd10_example <- openxlsx2::read_xlsx(
+  file = "data-raw/data_example_icd10.xlsx"
+) |>
+  tibble::tibble()
+
 usethis::use_data(icd10_example, overwrite = TRUE, compress = "xz")
 
 ### ICD11 example ----
-icd11_example <- openxlsx2::read_xlsx(file = "data-raw/data_example_icd11.xlsx")
+icd11_example <- openxlsx2::read_xlsx(
+  file = "data-raw/data_example_icd11.xlsx"
+) |>
+  tibble::tibble()
+
 usethis::use_data(icd11_example, overwrite = TRUE, compress = "xz")
